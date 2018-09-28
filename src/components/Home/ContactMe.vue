b6234e1b806fec3974f0d802d4191f1b186c638e
de la Cruz, Alejandro <a.de.la.cruz@accenture.com>
2018-09-27 22:45:01 -0500

    README Updated

---
+++ src/components/Home/ContactMe.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    Contact Me
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContactMe',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
