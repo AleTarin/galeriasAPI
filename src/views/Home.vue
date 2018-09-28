ccd934ead355c70b95a1a6bf81500bd2871a0814
de la Cruz, Alejandro <a.de.la.cruz@accenture.com>
2018-09-27 22:12:26 -0500

    Basic home structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PictureSlider/>
+    <AboutMe/>
+    <MyVideo/>
+    <QuoteSlider/>
+    <ContactMe/>
   </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// Import whatever component you are gone to use
+import PictureSlider from '@/components/Home/PictureSlider.vue'
+import QuoteSlider from '@/components/Home/QuoteSlider.vue'
+import AboutMe from '@/components/Home/AboutMe.vue'
+import MyVideo from '@/components/Home/MyVideo.vue'
+import ContactMe from '@/components/Home/ContactMe.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    PictureSlider,
+    QuoteSlider,
+    MyVideo,
+    AboutMe,
+    ContactMe
   }
 }
 </script>
